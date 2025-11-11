6917f19929d5c039b5ba79e8f68fad1ad9c907c0
nilsw13 <nils.wenting@laplateforme.io>
2025-11-11 19:45:09 +0100

    CLEAN : Clean unused import

--- src/routes/recetteRoutes.js
+++ src/routes/recetteRoutes.js
@@ -1,8 +1,8 @@
 import RecetteController from '../controllers/RecetteController.js';
-import recetteService from "../services/RecetteService.js";
 
 const recetteRoutes = (recetteService) => {
     const controller = new RecetteController(recetteService);
+
     return controller.getRouter();
 };
 
