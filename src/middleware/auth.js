8a9935df18f8e3a30d81a806809286416340ca69
nilsw13 <nils.wenting@laplateforme.io>
2025-11-12 14:33:56 +0100

    DEV: Setup auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+const authToken = (req, res, next) => {
+    //recupere le token depuis le header "Bearer <token>"
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // je recupere // <token>
+
+    if(!token) {
+        return res.status(401).json({
+            message : "Non autorisé"
+        });
+    }
+
+    try {
+        //verifie le token :
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }catch (error) {
+        return res.status(403).json({
+            message: "Non autorisé"
+        })
+    }
+}
+
+export default authToken;
