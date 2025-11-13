ab59f5c36b928c0c9a5423d12998dc35e7f52f87
nilsw13 <nils.wenting@laplateforme.io>
2025-11-13 20:34:10 +0100

    CLEAN: Remove unsued function

--- src/services/IngredientService.js
+++ src/services/IngredientService.js
@@ -3,10 +3,5 @@ class IngredientService {
         this.ingredientRepository = ingredientRepository;
     }
 
-    async save(ingredient)  {
-        if (!ingredient.nom) {
-            throw new Error('Nom obligatoire pour un ingrédient');
-        }
-        return await this.ingredientRepository.saveOne(ingredient);
-    }
+
 }
